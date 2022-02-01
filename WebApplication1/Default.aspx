7e0078a0950f1cbe8999706b14fa38cb5721ac34
MURAMASA2470 <keisuke226214@yahoo.co.jp>
2022-02-02 02:06:46 +0900

    init

---
+++ WebApplication1/Default.aspx
@@ -0,0 +1,74 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    	<style type="text/css">
+		
+		tr.Header th {
+			padding: 2px 18px 2px 18px;
+		}
+
+		tr.Header th a {
+			color: #000;
+			text-decoration: none;
+			font-weight: normal;
+		}
+
+		tr.Header th a:hover {
+			color: #f00;
+			text-decoration: underline;
+			font-weight: normal;
+		}
+
+		th.sortedAscHeader,
+		th.sortedDscHeader {
+			background-repeat: no-repeat;
+			background-position: right;
+			padding-right:18px;
+		}
+
+		th.sortedAscHeader {
+			content: '▼';
+		}
+
+		th.sortedDscHeader {
+			content: '▲';
+		}
+	</style>
+
+    <asp:Label runat="server" Text="部門" />
+    <asp:DropDownList ID="DropDown" runat="server" OnSelectedIndexChanged="DropDown_SelectedIndexChanged" />
+
+    <asp:GridView ID="GridList" runat="server" AutoGenerateColumns="false" AllowSorting="true" OnSorting="GridList_Sorting" AllowPaging="true" PageSize="3">
+	    <SortedAscendingHeaderStyle CssClass="sortedAscHeader"/>
+	    <SortedDescendingHeaderStyle CssClass="sortedDscHeader" />
+	    <HeaderStyle CssClass="Header" />
+        <Columns>
+            <asp:BoundField DataField="DATE" HeaderText="年月" DataFormatString="{0:yyyy/MM}"
+                SortExpression="DATE" AccessibleHeaderText="aaa" />
+            <asp:BoundField DataField="BMCD" HeaderText="部門コード"
+                SortExpression="BMCD" />
+            <asp:BoundField DataField="ORNM" HeaderText="発注先名"
+                SortExpression="ORNM" />
+            <%--<asp:TemplateField SortExpression="DATE">
+                <HeaderTemplate>年月</HeaderTemplate>
+                <ItemTemplate>
+                    <asp:Label Text='<%# DataBinder.Eval(Container.DataItem, "DATE")%>' runat="server" />
+                </ItemTemplate>
+            </asp:TemplateField>--%>
+<%--            <asp:TemplateField SortExpression="BMCD">
+                <HeaderTemplate>部門コード</HeaderTemplate>
+                <ItemTemplate>
+                    <asp:Label Text='<%# DataBinder.Eval(Container.DataItem, "BMCD")%>' runat="server" />
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField SortExpression="ORNM">
+                <HeaderTemplate>発注先名</HeaderTemplate>
+                <ItemTemplate>
+                    <asp:Label Text='<%# DataBinder.Eval(Container.DataItem, "ORNM")%>' runat="server" />
+                </ItemTemplate>
+            </asp:TemplateField>--%>
+        </Columns>
+    </asp:GridView>
+
+</asp:Content>
---
+++ WebApplication1/Default.aspx.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(this.DropDown.Items.Count == 0)
+            {
+                this.DropDown.Items.Add(new ListItem("全て", "0"));
+                this.DropDown.Items.Add(new ListItem("果物", "1"));
+                this.DropDown.Items.Add(new ListItem("野菜", "2"));
+                this.DropDown.Items.Add(new ListItem("精肉", "3"));
+            }
+
+            //var dt = new DataTable();
+            //dt.Columns.Add("DATE");
+            //dt.Columns.Add("BMCD");
+            //dt.Columns.Add("ORNM");
+
+            var list = new List<Dto>();
+
+            list.Add(new Dto { DATE = "202001", BMCD = "1", ORNM = "店９" });
+            list.Add(new Dto { DATE = "202002", BMCD = "2", ORNM = "店８" });
+            list.Add(new Dto { DATE = "202003", BMCD = "3", ORNM = "店７" });
+            list.Add(new Dto { DATE = "202003", BMCD = "4", ORNM = "店６" });
+            list.Add(new Dto { DATE = "202003", BMCD = "5", ORNM = "店５" });
+            list.Add(new Dto { DATE = "202003", BMCD = "6", ORNM = "店４" });
+            list.Add(new Dto { DATE = "202003", BMCD = "7", ORNM = "店３" });
+            list.Add(new Dto { DATE = "202003", BMCD = "8", ORNM = "店２" });
+            list.Add(new Dto { DATE = "202003", BMCD = "9", ORNM = "店１" });
+
+            this.GridList.DataSource = list;
+            this.GridList.DataBind();
+
+            if (Session["SortDirection"] == null)
+            {
+                Session["SortDirection"] = SortDirection.Descending;
+                Session["SortExpression"] = "BMCD";
+            }
+
+        }
+
+        protected void DropDown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                var selectedId = int.Parse(this.DropDown.SelectedValue);
+
+            } catch(Exception ex)
+            {
+                Response.Write(ex);
+            }
+        }
+
+
+
+        private class Dto
+        {
+            public string DATE { get; set; }
+
+            public string BMCD { get; set; }
+
+            public string ORNM { get; set; }
+        }
+
+        protected void GridList_Sorting(object sender, GridViewSortEventArgs e)
+        {
+
+            var sortDirection = (SortDirection) Session["SortDirection"];
+
+            var list = GridList.DataSource as List<Dto>;
+            if (sortDirection == SortDirection.Ascending)
+            {
+                GridList.DataSource = list.OrderByDescending(x => typeof(Dto).GetProperty(e.SortExpression).GetValue(x)).ToList();
+                sortDirection = SortDirection.Descending;
+            }
+            else
+            {
+                GridList.DataSource = list.OrderBy(x => typeof(Dto).GetProperty(e.SortExpression).GetValue(x)).ToList();
+                sortDirection = SortDirection.Ascending;
+            }
+
+            Session["SortExpression"] = e.SortExpression;
+            Session["SortDirection"] = sortDirection;
+
+            GridList.DataBind();
+        }
+
+        public SortDirection direction
+        {
+            get
+            {
+                if (ViewState["directionState"] == null)
+                {
+                    ViewState["directionState"] = SortDirection.Ascending;
+                }
+                return (SortDirection)ViewState["directionState"];
+            }
+            set
+            {
+                ViewState["directionState"] = value;
+            }
+        }
+    }
+}
